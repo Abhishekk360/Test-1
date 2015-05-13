99c115048cc2e134e322093b355076386f28fc24
Arpit Jha <arpitj@360logica.com>
2015-05-13 12:31:55 +0530

    Update Program.cs

--- TestConsole/TestConsole/Program.cs
+++ TestConsole/TestConsole/Program.cs
@@ -6,7 +6,7 @@ namespace TestConsole
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!ServerChanges1");
+			Console.WriteLine ("Hello World!ServerChanges1MyAddedServer");
 		}
 	}
 }
